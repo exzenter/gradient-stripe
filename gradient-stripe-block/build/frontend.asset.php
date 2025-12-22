10e7bac5e9f6a7d4746afff78141354c0c2bedf5
mitch <github@exzent.de>
2025-12-22 18:40:54 +0100

    Add .gitignore, @babel/runtime dependency, and build output

---
+++ gradient-stripe-block/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '9768ad779ca51563d186');
---
+++ gradient-stripe-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6dacd55d196dc88a00f5');
